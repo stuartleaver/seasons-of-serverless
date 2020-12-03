4c52bdd4b057a27fb9e4332fd723369a6e6df4d6
stuartleaver <stuartleaveruk@gmail.com>
2020-12-03 19:58:14 +0000

    Add frontend app
    Created the frontend UI in Vue.js. This is so that the results of the Azure Function can be displayed.

---
+++ 02-lovely-ladoos/app/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <LadooPredictor class="text-center" />
+  </div>
+</template>
+
+<script>
+import LadooPredictor from "./components/LadooPredictor.vue";
+
+export default {
+  name: "App",
+  components: {
+    LadooPredictor,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
