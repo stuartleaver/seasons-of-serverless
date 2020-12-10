406d9214673502b17c530f6e34d9803c5f58761d
stuartleaver <stuartleaveruk@gmail.com>
2020-12-10 21:10:52 +0000

    Fix UI test
    Corrected a test that was looking for the wrong app title

--- 03-the-longest-kebab/app/src/app/components/app.component.spec.ts
+++ 03-the-longest-kebab/app/src/app/components/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content h1').textContent).toContain('The Longest Kebab app is running!');
+    expect(compiled.querySelector('.content h1').textContent).toContain('The Longest Kebab');
   });
 });
