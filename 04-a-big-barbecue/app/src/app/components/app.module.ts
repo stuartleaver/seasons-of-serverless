b9d3633199fd9ed2e506fb54aab2ca38a5114347
stuartleaver <stuartleaveruk@gmail.com>
2020-12-17 15:28:26 +0000

    Create the Angular App
    Added the Angular app that will allow users to enter their details, and then display the costs of their barbecue.

---
+++ 04-a-big-barbecue/app/src/app/components/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { BigBarbecueComponent } from './big-barbecue/big-barbecue.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BigBarbecueComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ 04-a-big-barbecue/app/src/app/components/big-barbecue/barbecue-item.ts
@@ -0,0 +1,7 @@
+export interface BarbecueItem {
+  name: string;
+  cost: number;
+  quantity: number;
+  quantityUnit: string;
+  total: number;
+}
